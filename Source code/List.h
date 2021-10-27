c7cc9ba6d0d4b3e70961cac6081a0c885884bb34
Khngan <phankhanhnganngan@gmail.com>
2021-10-27 16:43:58 +0700

    Lay lai list.h

---
+++ Source code/List.h
@@ -0,0 +1,30 @@
+#ifndef LIST_H
+#define LIST_H
+#include "Author.h"
+#include "Article.h"
+#include "Journal.h"
+#include "Publisher.h"
+// Forward declaration class Publisher
+class Publisher;
+class List {
+    private:
+        Author *Auth;
+        Article *Art;
+        Journal *Jou;
+        Publisher *Pub;
+        int static Author_count;             //Biến đếm tổng số tác giả
+        int static Article_count;            //Biến đếm tổng số bài báo
+        int static Journal_count;            //Biến đếm tổng số tạp chí
+        int static Publisher_count;          //Biến đếm tổng số NXB
+    public:
+        friend class Publisher;
+        //List();
+        ~List();
+        void get_initialNum(ifstream &);
+        void List_getPublisher(ifstream &);
+        friend ostream &operator << (ostream &, const List &);
+
+};
+
+
+#endif
